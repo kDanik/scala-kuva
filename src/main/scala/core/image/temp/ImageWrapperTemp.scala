c5eb2662f0db1df3438a6e256250c92d07d0d9c6
kDanik <86569730+kDanik@users.noreply.github.com>
2023-03-16 11:12:53 +0100

    add one more source image and examples for it
    thanks to Paul Max Fischer for providing the image for this project

--- src/main/scala/core/image/temp/ImageWrapperTemp.scala
+++ src/main/scala/core/image/temp/ImageWrapperTemp.scala
@@ -14,7 +14,12 @@ import javax.imageio.ImageIO
  * Later this will be replaced by better image wrapper (probably immutable).
  */
 object ImageWrapperTemp {
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
+    generateCocktailImageExamples()
+    generateCocktailImageExamples()
+  }
+
+  def generateStrawberryImageExamples(): Unit = {
     applyOperationToEachPixelOfImage("src/main/resources/source/strawberry.png",
       "src/main/resources/result/grayscale_averaging_strawberry.png",
       (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.AVERAGING))
@@ -64,6 +69,56 @@ object ImageWrapperTemp {
       (color: Color) => ColorInversion.invertColor(color, false))
   }
 
+  def generateCocktailImageExamples(): Unit = {
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_averaging_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.AVERAGING))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_luma_bt601_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.LUMA_BT601))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_luma_bt709_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.LUMA_BT709))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_decomposition_max_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, algorithm = GrayscaleConversionAlgorithm.DECOMPOSITION_MAX))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_decomposition_min_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.DECOMPOSITION_MIN))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_desaturation_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.DESATURATION))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_single_color_channel_red_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.SINGLE_COLOR_CHANNEL_RED))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_single_color_channel_green_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.SINGLE_COLOR_CHANNEL_GREEN))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_single_color_channel_blue_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.SINGLE_COLOR_CHANNEL_BLUE))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_lightness_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.LIGHTNESS))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/grayscale_lightness_hsl_cocktail.png",
+      (color: Color) => GrayscaleColorConversion.applyGrayscale(color, GrayscaleConversionAlgorithm.LIGHTNESS_HSL))
+
+    applyOperationToEachPixelOfImage("src/main/resources/source/cocktail.png",
+      "src/main/resources/result/color_inversion_cocktail.png",
+      (color: Color) => ColorInversion.invertColor(color, false))
+  }
+
   def applyOperationToEachPixelOfImage(sourceImagePath: String, resultedImagePath: String, colorOperation: Color => ColorRgba): Unit = {
     val bufferedImage = loadImage(sourceImagePath)
     val width = bufferedImage.getWidth
@@ -75,6 +130,29 @@ object ImageWrapperTemp {
       val awtColor = java.awt.Color(bufferedImage.getRGB(x, y), true)
 
       resultedImage.setRGB(x, y, colorOperation(ColorRgba.apply(awtColor)).asAwtColor.getRGB)
+    }
+
+    writeImage(resultedImagePath, resultedImage)
+  }
+
+  def combineTwoImageInOne(sourceBackgroundImagePath: String, sourceForegroundImagePath: String, resultedImagePath: String, colorOperation: (ColorRgba, ColorRgba) => ColorRgba): Unit = {
+    val bufferedImageBackground = loadImage(sourceBackgroundImagePath)
+    val bufferedImageForeground = loadImage(sourceForegroundImagePath)
+
+    // this test method assumes that 2 images have same size. For real life blending also coordinates of images should be used
+    // (to combine images with different size
+    val width = bufferedImageBackground.getWidth
+    val height = bufferedImageBackground.getHeight
+
+    // it also assumes images have same type
+    val resultedImage = BufferedImage(width, height, bufferedImageBackground.getType);
+
+
+    for (x <- 0 until width; y <- 0 until height) {
+      val awtColorBackground = java.awt.Color(bufferedImageBackground.getRGB(x, y), true)
+      val awtColorForeground = java.awt.Color(bufferedImageForeground.getRGB(x, y), true)
+
+      resultedImage.setRGB(x, y, colorOperation(ColorRgba.apply(awtColorBackground), ColorRgba.apply(awtColorForeground)).asAwtColor.getRGB)
     }
 
     writeImage(resultedImagePath, resultedImage)
