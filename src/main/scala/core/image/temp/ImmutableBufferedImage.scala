9cf46d8edc2c3b8da1406eec0bd9c8010f11cb26
kDanik <86569730+kDanik@users.noreply.github.com>
2023-03-17 22:30:11 +0100

    add some documentation to immutable image and pixel

--- src/main/scala/core/image/temp/ImmutableBufferedImage.scala
+++ src/main/scala/core/image/temp/ImmutableBufferedImage.scala
@@ -5,26 +5,52 @@ import core.color.types.{Color, ColorRgba}
 
 import java.awt.image.BufferedImage
 
+/**
+ * This class is a primitive implementation of immutable image.
+ * Its idea is to replace java mutable BufferedImage in code for image processing,
+ * and later convert this class back to BufferedImage (if needed) for saving
+ *
+ * @param imageRaster 2D matrix with pixels for this image
+ */
 final case class ImmutableBufferedImage(imageRaster: Vector[Vector[Pixel]]) {
+  /**
+   * height of this image in pixels
+   */
   lazy val Height = imageRaster.length;
 
+  /**
+   * width of this image in pixels
+   */
   lazy val Width = imageRaster.headOption match {
     case Some(value) => value.length
     case None => 0
   }
 
+  /**
+   * Sets / Replaces one pixel in ImmutableBufferedImage
+   *
+   * @param pixel pixel that should be replaced in ImmutableBufferedImage
+   * @return new ImmutableBufferedImage after changes
+   */
   def setPixel(pixel: Pixel): ImmutableBufferedImage = {
     if (isPositionInBounds(pixel.x, pixel.y)) {
       this.copy(imageRaster.updated(pixel.y, imageRaster(pixel.y).updated(pixel.x, pixel)))
     } else this
   }
 
+  /**
+   * Return Option with pixel for given coordinates.
+   * If coordinates are out of bound empty Option will be returned.
+   */
   def getPixel(x: Int, y: Int): Option[Pixel] = {
     if (isPositionInBounds(x, y)) {
       Option.apply(imageRaster(y)(x))
     } else Option.empty
   }
 
+  /**
+   * Creates BufferedImage (java awt) using data of this object
+   */
   def asBufferedImage: BufferedImage = {
     if (Height == 0 || Width == 0) {
       BufferedImage(Width, Height, BufferedImage.TYPE_INT_RGB)
@@ -33,10 +59,12 @@ final case class ImmutableBufferedImage(imageRaster: Vector[Vector[Pixel]]) {
     }
   }
 
+  /**
+   * @return true if position is in bounds of this ImmutableBufferedImage, otherwise false
+   */
   def isPositionInBounds(x: Int, y: Int): Boolean = {
     ImmutableBufferedImage.isPositionNonNegative(x, y) && (x < Width && y < Height)
   }
-
 
   private def imageRasterContentAsBufferedImage: BufferedImage = {
     val bufferedImage = BufferedImage(Width, Height, BufferedImage.TYPE_INT_RGB)
@@ -48,13 +76,18 @@ final case class ImmutableBufferedImage(imageRaster: Vector[Vector[Pixel]]) {
 }
 
 object ImmutableBufferedImage {
+  /**
+   * Creates ImmutableBufferedImage with given size (filled with (0,0,0,0) color)
+   */
   def apply(height: Int, width: Int): Option[ImmutableBufferedImage] = {
     if (isPositionNonNegative(width, height)) {
       Option.apply(ImmutableBufferedImage(Vector.tabulate(height, width)((y, x) => Pixel(x, y, ColorRgba.apply(0, 0, 0, 0)))))
     } else Option.empty
   }
 
-
+  /**
+   * CreatesImmutableBufferedImage using data (size, pixel data) of BufferedImage (java awt)
+   */
   def apply(bufferedImage: BufferedImage): ImmutableBufferedImage = {
     def pixelFromBufferedImagePosition(x: Int, y: Int, bufferedImage: BufferedImage): Pixel = Pixel(x, y, ColorRgba.fromRgbaInt(bufferedImage.getRGB(x, y)))
 
--- src/main/scala/core/image/temp/Pixel.scala
+++ src/main/scala/core/image/temp/Pixel.scala
@@ -3,4 +3,11 @@ package core.image.temp
 
 import core.color.types.{Color, ColorRgba}
 
+/**
+ * This class represents on pixel on image
+ *
+ * @param x     x coordinate on image
+ * @param y     y coordinate on image
+ * @param color color of this pixel
+ */
 final case class Pixel(x: Int, y: Int, color: Color)
