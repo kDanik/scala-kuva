21e58dcc905be8f5dc5270ae33844df9a42bf435
kDanik <86569730+kDanik@users.noreply.github.com>
2023-04-10 13:46:56 +0200

    add extra blending examples for debugging purposes
    add extra blending examples for debugging purposes

--- src/main/scala/examples/BlendImageExamplesMain.scala
+++ src/main/scala/examples/BlendImageExamplesMain.scala
@@ -7,31 +7,150 @@ import examples.util.{ImageExampleFileUtil, ImageExampleOperations}
 
 object BlendImageExamplesMain {
   def main(args: Array[String]): Unit = {
+    blendExamplesAbstractGraphics()
+    blendExamplesGradients()
+  }
+
+  private def blendExamplesAbstractGraphics(): Unit = {
+    val backgroundImagePath = "src/main/resources/source/blend/abstract_background.png"
+    val foregroundImagePath = "src/main/resources/source/blend/abstract_foreground.png"
 
     blendTwoImages(
       BlendMode.SIMPLE_ALPHA_COMPOSITING,
-      "src/main/resources/result/blend/blend_simple_alpha_compositing.png")
-    blendTwoImages(BlendMode.MULTIPLY, "src/main/resources/result/blend/blend_multiply.png")
-    blendTwoImages(BlendMode.SCREEN, "src/main/resources/result/blend/blend_screen.png")
-    blendTwoImages(BlendMode.OVERLAY, "src/main/resources/result/blend/blend_overlay.png")
-    blendTwoImages(BlendMode.HARD_LIGHT, "src/main/resources/result/blend/blend_hard_light.png")
-    blendTwoImages(BlendMode.SOFT_LIGHT, "src/main/resources/result/blend/blend_soft_light.png")
-    blendTwoImages(BlendMode.COLOR_DODGE, "src/main/resources/result/blend/blend_color_dodge.png")
+      "src/main/resources/result/blend/blend_simple_alpha_compositing.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.MULTIPLY,
+      "src/main/resources/result/blend/blend_multiply.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.SCREEN,
+      "src/main/resources/result/blend/blend_screen.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.OVERLAY,
+      "src/main/resources/result/blend/blend_overlay.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.HARD_LIGHT,
+      "src/main/resources/result/blend/blend_hard_light.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.SOFT_LIGHT,
+      "src/main/resources/result/blend/blend_soft_light.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.COLOR_DODGE,
+      "src/main/resources/result/blend/blend_color_dodge.png",
+      backgroundImagePath,
+      foregroundImagePath)
     blendTwoImages(
       BlendMode.LINEAR_DODGE,
-      "src/main/resources/result/blend/blend_linear_dodge.png")
-    blendTwoImages(BlendMode.COLOR_BURN, "src/main/resources/result/blend/blend_color_burn.png")
-    blendTwoImages(BlendMode.LINEAR_BURN, "src/main/resources/result/blend/blend_linear_burn.png")
-    blendTwoImages(BlendMode.VIVID_LIGHT, "src/main/resources/result/blend/blend_vivid_light.png")
+      "src/main/resources/result/blend/blend_linear_dodge.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.COLOR_BURN,
+      "src/main/resources/result/blend/blend_color_burn.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.LINEAR_BURN,
+      "src/main/resources/result/blend/blend_linear_burn.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.VIVID_LIGHT,
+      "src/main/resources/result/blend/blend_vivid_light.png",
+      backgroundImagePath,
+      foregroundImagePath)
     blendTwoImages(
       BlendMode.LINEAR_LIGHT,
-      "src/main/resources/result/blend/blend_linear_light.png")
+      "src/main/resources/result/blend/blend_linear_light.png",
+      backgroundImagePath,
+      foregroundImagePath)
   }
 
-  private def blendTwoImages(blendMode: BlendMode, resultedImagePath: String): Unit = {
+  private def blendExamplesGradients(): Unit = {
+    val backgroundImagePath = "src/main/resources/source/blend/gradient_base.png"
+    val foregroundImagePath = "src/main/resources/source/blend/gradient_blend.png"
+
+    blendTwoImages(
+      BlendMode.SIMPLE_ALPHA_COMPOSITING,
+      "src/main/resources/result/blend/gradient/blend_simple_alpha_compositing.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.MULTIPLY,
+      "src/main/resources/result/blend/gradient/blend_multiply.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.SCREEN,
+      "src/main/resources/result/blend/gradient/blend_screen.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.OVERLAY,
+      "src/main/resources/result/blend/gradient/blend_overlay.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.HARD_LIGHT,
+      "src/main/resources/result/blend/gradient/blend_hard_light.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.SOFT_LIGHT,
+      "src/main/resources/result/blend/gradient/blend_soft_light.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.COLOR_DODGE,
+      "src/main/resources/result/blend/gradient/blend_color_dodge.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.LINEAR_DODGE,
+      "src/main/resources/result/blend/gradient/blend_linear_dodge.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.COLOR_BURN,
+      "src/main/resources/result/blend/gradient/blend_color_burn.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.LINEAR_BURN,
+      "src/main/resources/result/blend/gradient/blend_linear_burn.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.VIVID_LIGHT,
+      "src/main/resources/result/blend/gradient/blend_vivid_light.png",
+      backgroundImagePath,
+      foregroundImagePath)
+    blendTwoImages(
+      BlendMode.LINEAR_LIGHT,
+      "src/main/resources/result/blend/gradient/blend_linear_light.png",
+      backgroundImagePath,
+      foregroundImagePath)
+  }
+
+  private def blendTwoImages(
+      blendMode: BlendMode,
+      resultedImagePath: String,
+      backgroundImagePath: String,
+      foregroundImagePath: String): Unit = {
     val resultedImage = ImageExampleOperations.combineTwoImageInOne(
-      ImageExampleFileUtil.loadImage("src/main/resources/source/blend/abstract_background.png"),
-      ImageExampleFileUtil.loadImage("src/main/resources/source/blend/abstract_foreground.png"),
+      ImageExampleFileUtil.loadImage(backgroundImagePath),
+      ImageExampleFileUtil.loadImage(foregroundImagePath),
       (background: Color, foreground: Color) =>
         ColorBlending.blend(background, foreground, blendMode))
 
--- src/main/scala/examples/RandomisedBlendImageExamplesMain.scala
+++ src/main/scala/examples/RandomisedBlendImageExamplesMain.scala
@@ -11,7 +11,7 @@ import examples.util.{ImageExampleFileUtil, ImageExampleOperations}
  */
 object RandomisedBlendImageExamplesMain {
   def main(args: Array[String]): Unit = {
-    val resultedImage = ImageExampleOperations.combineTwoImageInOne(
+    val resultedImageAbstractGraphics = ImageExampleOperations.combineTwoImageInOne(
       ImageExampleFileUtil.loadImage("src/main/resources/source/blend/abstract_background.png"),
       ImageExampleFileUtil.loadImage("src/main/resources/source/blend/abstract_foreground.png"),
       (background: Color, foreground: Color) =>
@@ -19,6 +19,17 @@ object RandomisedBlendImageExamplesMain {
 
     ImageExampleFileUtil.writeImage(
       "src/main/resources/result/blend/blend_dissolve.png",
-      resultedImage)
+      resultedImageAbstractGraphics)
+
+    val resultedImageGradient = ImageExampleOperations.combineTwoImageInOne(
+      ImageExampleFileUtil.loadImage("src/main/resources/source/blend/gradient_base.png"),
+      ImageExampleFileUtil.loadImage("src/main/resources/source/blend/gradient_blend.png"),
+      (background: Color, foreground: Color) =>
+        ColorBlending.blend(background, foreground, BlendMode.DISSOLVE))
+
+    ImageExampleFileUtil.writeImage(
+      "src/main/resources/result/blend/gradient/blend_dissolve.png",
+      resultedImageGradient)
   }
+
 }
