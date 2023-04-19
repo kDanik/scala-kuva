6e87bf63d3e931e04e2176f3b0a797c223f8d503
kDanik <86569730+kDanik@users.noreply.github.com>
2023-04-19 15:06:23 +0200

    cleanup and renamings for image scaling

--- src/main/scala/core/image/operations/scale/ImageScaling.scala
+++ src/main/scala/core/image/operations/scale/ImageScaling.scala
@@ -61,10 +61,6 @@ object ImageScaling {
    * interpolation uses 4 nearest neighbors to determine the output.
    *
    * It is not recommended to use bilinear interpolation for downscaling.
-   * @param sourceImage
-   * @param targetWidth
-   * @param targetHeight
-   * @return
    */
   def scaleWithBilinearInterpolation(
       sourceImage: ImmutableBufferedImage,
@@ -90,76 +86,99 @@ object ImageScaling {
 
     // map each pixel of target image to pixel of source image
     targetImage.mapPixels((pixel: Pixel) => {
-      // calculate position of this pixel on source image
-      val sourceX = pixel.x.floatValue() / targetWidth * sourceWidth
-      val sourceY = pixel.y.floatValue() / targetHeight * sourceHeight
-
-      // convert sourceX and sourceY to position (int value) of closest pixel (bottom left pixel in bilinear interpolation)
-      val normalizedSourceX =
-        if (sourceX.intValue >= sourceWidth - 1) sourceWidth - 2 else sourceX.intValue
-      val normalizedSourceY =
-        if (sourceY.intValue >= sourceHeight - 1) sourceHeight - 2 else sourceY.intValue
-
-      // Get values of surrounding pixels (top left, top right, bottom left, bottom right)
-      val q11 = sourceImage.getPixel(normalizedSourceX, normalizedSourceY).get.color.asColorRgba
-      val q12 =
-        sourceImage.getPixel(normalizedSourceX, normalizedSourceY + 1).get.color.asColorRgba
-      val q21 =
-        sourceImage.getPixel(normalizedSourceX + 1, normalizedSourceY).get.color.asColorRgba
-      val q22 =
-        sourceImage.getPixel(normalizedSourceX + 1, normalizedSourceY + 1).get.color.asColorRgba
-
-      // calculate value for each channel using bilinear interpolation
-      val red = Interpolation.bilinearInterpolation(
-        sourceX,
-        sourceY,
-        normalizedSourceX,
-        normalizedSourceY,
-        normalizedSourceX + 1,
-        normalizedSourceY + 1,
-        q11.redAsFloat,
-        q12.redAsFloat,
-        q21.redAsFloat,
-        q22.redAsFloat)
-      val green = Interpolation.bilinearInterpolation(
-        sourceX,
-        sourceY,
-        normalizedSourceX,
-        normalizedSourceY,
-        normalizedSourceX + 1,
-        normalizedSourceY + 1,
-        q11.greenAsFloat,
-        q12.greenAsFloat,
-        q21.greenAsFloat,
-        q22.greenAsFloat)
-      val blue = Interpolation.bilinearInterpolation(
-        sourceX,
-        sourceY,
-        normalizedSourceX,
-        normalizedSourceY,
-        normalizedSourceX + 1,
-        normalizedSourceY + 1,
-        q11.blueAsFloat,
-        q12.blueAsFloat,
-        q21.blueAsFloat,
-        q22.blueAsFloat)
-      val alpha = Interpolation.bilinearInterpolation(
-        sourceX,
-        sourceY,
-        normalizedSourceX,
-        normalizedSourceY,
-        normalizedSourceX + 1,
-        normalizedSourceY + 1,
-        q11.alphaAsFloat,
-        q12.alphaAsFloat,
-        q21.alphaAsFloat,
-        q22.alphaAsFloat)
 
       // assign to this pixel resulting color
-      pixel.copy(color = ColorRgba(red, green, blue, alpha))
+      pixel.copy(color = calculateSinglePixelColorWithBilinearInterpolation(
+        sourceImage,
+        targetWidth,
+        targetHeight,
+        pixel,
+        sourceWidth,
+        sourceHeight))
     })
   }
 
+  private def calculateSinglePixelColorWithBilinearInterpolation(
+      sourceImage: ImmutableBufferedImage,
+      targetWidth: Int,
+      targetHeight: Int,
+      pixel: Pixel,
+      sourceWidth: Int,
+      sourceHeight: Int): ColorRgba = {
+    // calculate position of this pixel on source image
+    val sourceX = pixel.x.floatValue() / targetWidth * sourceWidth
+    val sourceY = pixel.y.floatValue() / targetHeight * sourceHeight
+
+    // convert sourceX and sourceY to position (int value) of closest pixel (bottom left pixel in bilinear interpolation)
+    val normalizedSourceX =
+      if (sourceX.intValue >= sourceWidth - 1) sourceWidth - 2 else sourceX.intValue
+    val normalizedSourceY =
+      if (sourceY.intValue >= sourceHeight - 1) sourceHeight - 2 else sourceY.intValue
+
+    // Get values of surrounding pixels (top left, top right, bottom left, bottom right)
+    val q11 = sourceImage.getPixel(normalizedSourceX, normalizedSourceY).get.color.asColorRgba
+    val q12 =
+      sourceImage.getPixel(normalizedSourceX, normalizedSourceY + 1).get.color.asColorRgba
+    val q21 =
+      sourceImage.getPixel(normalizedSourceX + 1, normalizedSourceY).get.color.asColorRgba
+    val q22 =
+      sourceImage.getPixel(normalizedSourceX + 1, normalizedSourceY + 1).get.color.asColorRgba
+
+    // calculate value for each channel using bilinear interpolation
+    val red = Interpolation.bilinearInterpolation(
+      sourceX,
+      sourceY,
+      normalizedSourceX,
+      normalizedSourceY,
+      normalizedSourceX + 1,
+      normalizedSourceY + 1,
+      q11.redAsFloat,
+      q12.redAsFloat,
+      q21.redAsFloat,
+      q22.redAsFloat)
+    val green = Interpolation.bilinearInterpolation(
+      sourceX,
+      sourceY,
+      normalizedSourceX,
+      normalizedSourceY,
+      normalizedSourceX + 1,
+      normalizedSourceY + 1,
+      q11.greenAsFloat,
+      q12.greenAsFloat,
+      q21.greenAsFloat,
+      q22.greenAsFloat)
+    val blue = Interpolation.bilinearInterpolation(
+      sourceX,
+      sourceY,
+      normalizedSourceX,
+      normalizedSourceY,
+      normalizedSourceX + 1,
+      normalizedSourceY + 1,
+      q11.blueAsFloat,
+      q12.blueAsFloat,
+      q21.blueAsFloat,
+      q22.blueAsFloat)
+    val alpha = Interpolation.bilinearInterpolation(
+      sourceX,
+      sourceY,
+      normalizedSourceX,
+      normalizedSourceY,
+      normalizedSourceX + 1,
+      normalizedSourceY + 1,
+      q11.alphaAsFloat,
+      q12.alphaAsFloat,
+      q21.alphaAsFloat,
+      q22.alphaAsFloat)
+
+    ColorRgba(red, green, blue, alpha)
+  }
+
+  /**
+   * Scales image to desired proportions, using bilinear interpolation algorithm. Bilinear
+   * interpolation uses 4 nearest neighbors to determine the output.
+   *
+   * It is not recommended to use bilinear interpolation for downscaling.
+   */
   def scaleWithBicubicInterpolation(
       sourceImage: ImmutableBufferedImage,
       targetWidth: Int,
@@ -178,53 +197,66 @@ object ImageScaling {
       targetImage: ImmutableBufferedImage,
       targetWidth: Int,
       targetHeight: Int): ImmutableBufferedImage = {
-
     val sourceWidth = sourceImage.Width
     val sourceHeight = sourceImage.Height
 
-    // map each pixel of target image to pixel of source image
+    // calculate color for each pixel of target image, using pixels from source image (and bicubic interpolation)
     targetImage.mapPixels((pixel: Pixel) => {
-
-      // calculate position of this pixel on source image
-      val sourceX = pixel.x.floatValue() / targetWidth * sourceWidth
-      val sourceY = pixel.y.floatValue() / targetHeight * sourceHeight
-
-      if (sourceX.intValue >= sourceWidth - 2 || sourceY.intValue >= sourceHeight - 2 || sourceY.intValue == 0 || sourceX.intValue == 0) {
-        pixel.copy(color = ColorRgba(0, 0, 0, 255))
-      } else {
-
-        val normalizedSourceX = sourceX.intValue
-        val normalizedSourceY = sourceY.intValue
-
-        // create 2D array (4x4) of pixels around normalizedSourceX and normalizedSourceY
-        val nearestPixelColors = ArraySeq.tabulate(4, 4)((y: Int, x: Int) =>
-          sourceImage
-            .getPixel(normalizedSourceX - 1 + x, normalizedSourceY - 1 + y)
-            .get
-            .color
-            .asColorRgba)
-
-        // calculate value for each channel using bilinear interpolation
-        val red = Interpolation.bicubicInterpolation(
-          sourceX - normalizedSourceX,
-          sourceY - normalizedSourceY,
-          nearestPixelColors.map(_.map(_.redAsFloat)))
-        val green = Interpolation.bicubicInterpolation(
-          sourceX - normalizedSourceX,
-          sourceY - normalizedSourceY,
-          nearestPixelColors.map(_.map(_.greenAsFloat)))
-        val blue = Interpolation.bicubicInterpolation(
-          sourceX - normalizedSourceX,
-          sourceY - normalizedSourceY,
-          nearestPixelColors.map(_.map(_.blueAsFloat)))
-        val alpha = Interpolation.bicubicInterpolation(
-          sourceX - normalizedSourceX,
-          sourceY - normalizedSourceY,
-          nearestPixelColors.map(_.map(_.alphaAsFloat)))
-
-        // assign to this pixel resulting color
-        pixel.copy(color = ColorRgba(red, green, blue, alpha))
-      }
+      pixel.copy(color = calculateSinglePixelColorWithBicubicInterpolation(
+        sourceImage,
+        targetWidth,
+        targetHeight,
+        pixel,
+        sourceWidth,
+        sourceHeight))
     })
+  }
+}
+
+private def calculateSinglePixelColorWithBicubicInterpolation(
+    sourceImage: ImmutableBufferedImage,
+    targetWidth: Int,
+    targetHeight: Int,
+    pixel: Pixel,
+    sourceWidth: Int,
+    sourceHeight: Int): ColorRgba = {
+  // calculate position of this pixel on source image
+  val sourceX = pixel.x.floatValue() / targetWidth * sourceWidth
+  val sourceY = pixel.y.floatValue() / targetHeight * sourceHeight
+
+  if (sourceX.intValue >= sourceWidth - 2 || sourceY.intValue >= sourceHeight - 2 || sourceY.intValue == 0 || sourceX.intValue == 0) {
+    // TODO better handling of border pixels should be implemented
+    ColorRgba(0, 0, 0, 255)
+  } else {
+    val normalizedSourceX = sourceX.intValue
+    val normalizedSourceY = sourceY.intValue
+
+    // create 2D array (4x4) of pixels around normalizedSourceX and normalizedSourceY
+    val nearestPixelColors = ArraySeq.tabulate(4, 4)((y: Int, x: Int) =>
+      sourceImage
+        .getPixel(normalizedSourceX - 1 + x, normalizedSourceY - 1 + y)
+        .get
+        .color
+        .asColorRgba)
+
+    // calculate value for each channel using bilinear interpolation
+    val red = Interpolation.bicubicInterpolation(
+      sourceX - normalizedSourceX,
+      sourceY - normalizedSourceY,
+      nearestPixelColors.map(_.map(_.redAsFloat)))
+    val green = Interpolation.bicubicInterpolation(
+      sourceX - normalizedSourceX,
+      sourceY - normalizedSourceY,
+      nearestPixelColors.map(_.map(_.greenAsFloat)))
+    val blue = Interpolation.bicubicInterpolation(
+      sourceX - normalizedSourceX,
+      sourceY - normalizedSourceY,
+      nearestPixelColors.map(_.map(_.blueAsFloat)))
+    val alpha = Interpolation.bicubicInterpolation(
+      sourceX - normalizedSourceX,
+      sourceY - normalizedSourceY,
+      nearestPixelColors.map(_.map(_.alphaAsFloat)))
+
+    ColorRgba(red, green, blue, alpha)
   }
 }
--- src/main/scala/core/support/math/interpolation/Interpolation.scala
+++ src/main/scala/core/support/math/interpolation/Interpolation.scala
@@ -11,19 +11,19 @@ object Interpolation {
    * Calculates value for input point (1D), using liner interpolation.
    * @param inputX
    *   Input coordinate
-   * @param x1
+   * @param x0
    *   Coordinate of point 1
-   * @param q1
+   * @param q0
    *   value of point 1
-   * @param x2
+   * @param x1
    *   Coordinate of point 2
-   * @param q2
+   * @param q1
    *   value of point 2
    * @return
    *   calculated value
    */
-  def linearInterpolation(inputX: Float, x1: Float, q1: Float, x2: Float, q2: Float): Float = {
-    q1 + ((q2 - q1) / (x2 - x1)) * (inputX - x1)
+  def linearInterpolation(inputX: Float, x0: Float, q0: Float, x1: Float, q1: Float): Float = {
+    q0 + ((q1 - q0) / (x1 - x0)) * (inputX - x0)
   }
 
   /**
@@ -33,62 +33,63 @@ object Interpolation {
    *   X of input point
    * @param inputY
    *   Y of input point
-   * @param x1
+   * @param x0
    *   X of left points
-   * @param y1
+   * @param y0
    *   Y of top points
-   * @param x2
+   * @param x1
    *   X of right points
-   * @param y2
+   * @param y1
    *   Y of bottom points
+   * @param q00
+   *   value of closest left top point (x0, y0)
+   * @param q01
+   *   value of closest left bottom point (x0, y1)
+   * @param q10
+   *   value of closest right top point (x0, y0)
    * @param q11
-   *   value of closest left top point (x1, y1)
-   * @param q12
-   *   value of closest left bottom point (x1, y2)
-   * @param q21
-   *   value of closest right top point (x2, y1)
-   * @param q22
-   *   value of closest right bottom point (x2, y2)
+   *   value of closest right bottom point (x0, y1)
    * @return
    *   calculated value
    */
   def bilinearInterpolation(
       inputX: Float,
       inputY: Float,
+      x0: Float,
+      y0: Float,
       x1: Float,
       y1: Float,
-      x2: Float,
-      y2: Float,
-      q11: Float,
-      q12: Float,
-      q21: Float,
-      q22: Float): Float = {
+      q00: Float,
+      q01: Float,
+      q10: Float,
+      q11: Float): Float = {
+    // this linear interpolation uses 2 points with same y (y0) and different x
+    val linearInterpolationX1 = linearInterpolation(inputX, x0, q00, x1, q10)
+
     // this linear interpolation uses 2 points with same y (y1) and different x
-    val linearInterpolationX1 = linearInterpolation(inputX, x1, q11, x2, q21)
+    val linearInterpolationX2 = linearInterpolation(inputX, x0, q01, x1, q11)
 
-    // this linear interpolation uses 2 points with same y (y2) and different x
-    val linearInterpolationX2 = linearInterpolation(inputX, x1, q12, x2, q22)
-
-    // this linear interpolation uses results of x1 and x2 interpolations and performs linear interpolation using y1, y2 coordinates
-    linearInterpolation(inputY, y1, linearInterpolationX1, y2, linearInterpolationX2)
+    // this linear interpolation uses results of x0 and x0 interpolations and performs linear interpolation using y0, y1 coordinates
+    linearInterpolation(inputY, y0, linearInterpolationX1, y1, linearInterpolationX2)
   }
 
   /**
    * This implementation of cubic Interpolation using weight distribution from OpenCV. <br>It is
-   * assumed that: <br>1. Distance between adjacent points with weights (q0, q1, q2, q3) is always
-   * 1 (means distance between q2 and q3 is 1). <br>2. Input x position is in range 0 to 1, where
-   * 0 is position of point with q1 value and 1 is position of point with q2 value.
+   * assumed that: <br>1. Distance between adjacent points with weights (q0, q0, q1, q3) is always
+   * 1 (means distance between q1 and q3 is 1). <br>2. Input x position is in range 0 to 1, where
+   * 0 is position of point with q0 value and 1 is position of point with q1 value.
    */
   def cubicInterpolationOpenCV(x: Float, values: ArraySeq[Float]): Float = {
-    val w0 =
-      ((cubicInterpolationOpenCvConstant * (x + 1) - 5 * cubicInterpolationOpenCvConstant) * (x + 1) + 8 * cubicInterpolationOpenCvConstant) * (x + 1) - 4 * cubicInterpolationOpenCvConstant
-    val w1 =
+    val weight0 =
+      ((cubicInterpolationOpenCvConstant * (x + 1) - 5 * cubicInterpolationOpenCvConstant) * (x + 1) +
+        8 * cubicInterpolationOpenCvConstant) * (x + 1) - 4 * cubicInterpolationOpenCvConstant
+    val weight1 =
       ((cubicInterpolationOpenCvConstant + 2) * x - (cubicInterpolationOpenCvConstant + 3)) * x * x + 1
-    val w2 =
+    val weight2 =
       ((cubicInterpolationOpenCvConstant + 2) * (1 - x) - (cubicInterpolationOpenCvConstant + 3)) * (1 - x) * (1 - x) + 1
-    val w3 = 1f - w0 - w1 - w2
+    val weight3 = 1f - weight0 - weight1 - weight2
 
-    values(0) * w0 + values(1) * w1 + values(2) * w2 + values(3) * w3
+    values(0) * weight0 + values(1) * weight1 + values(2) * weight2 + values(3) * weight3
   }
 
   /**
@@ -100,18 +101,18 @@ object Interpolation {
       inputX: Float,
       values: ArraySeq[Float],
       positions: ArraySeq[Float]): Float = {
-    val w0 =
+    val weight0 =
       ((inputX - positions(1)) * (inputX - positions(2)) * (inputX - positions(3))) / ((positions(
         0) - positions(1)) * (positions(0) - positions(2)) * (positions(0) - positions(3)))
-    val w1 =
+    val weight1 =
       ((inputX - positions(0)) * (inputX - positions(2)) * (inputX - positions(3))) / ((positions(
         1) - positions(0)) * (positions(1) - positions(2)) * (positions(1) - positions(3)))
-    val w2 =
+    val weight2 =
       ((inputX - positions(0)) * (inputX - positions(1)) * (inputX - positions(3))) / ((positions(
         2) - positions(0)) * (positions(2) - positions(1)) * (positions(2) - positions(3)))
-    val w3 = 1f - w0 - w1 - w2
+    val weight3 = 1f - weight0 - weight1 - weight2
 
-    values(0) * w0 + values(1) * w1 + values(2) * w2 + values(3) * w3
+    values(0) * weight0 + values(1) * weight1 + values(2) * weight2 + values(3) * weight3
   }
 
   def bicubicInterpolation(
