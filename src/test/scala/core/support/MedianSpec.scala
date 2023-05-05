d21bd07e560ef090b19dd71a0ede4094997b5e56
kDanik <86569730+kDanik@users.noreply.github.com>
2023-05-05 18:49:22 +0200

    add tests for median calculation

---
+++ src/test/scala/core/support/MedianSpec.scala
@@ -0,0 +1,38 @@
+package com.example
+package core.support
+
+import core.support.math.interpolation.MedianSeq
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class MedianSpec extends AnyFlatSpec {
+  "Median" should "correctly calculate median for array with odd number of elements" in {
+    val seq = Seq(1f, 2.3f, 4f, 3f, 4.5f)
+
+    assert(seq.median.get == 3.0f)
+  }
+
+  "Median" should "correctly calculate median for array with even number of elements" in {
+    val seq = Seq(1f, 2.3f, 3f, 4.5f)
+
+    assert(seq.median.get == 2.65f)
+  }
+
+  "Median" should "correctly calculate median for array with 2 elements" in {
+    val seq = Seq(1f, 4.5f)
+
+    assert(seq.median.get == 2.75f)
+  }
+
+  "Median" should "correctly calculate median for array with 1 element" in {
+    val seq = Seq(1f)
+
+    assert(seq.median.get == 1f)
+  }
+
+  "Median" should "return empty option for empty array" in {
+    val seq: Seq[Double] = Seq.empty
+
+    assert(seq.median.isEmpty)
+  }
+}
